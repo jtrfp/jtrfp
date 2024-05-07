0741cded507316f619bcb53579b9528f3fd020b0
Chuck Ritola <cjritola@gmail.com>
2024-05-07 15:55:37 -0400

    Initial creation of RAWAdapterTest.

---
+++ src/test/java/org/jtrfp/jtrfp/raw/jfdt/RAWAdapterTest.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * This file is part of jTRFP
+ * Copyright (c) 2012-2024. See commit history for copyright owners.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial implementation
+ ******************************************************************************/
+
+package org.jtrfp.jtrfp.raw.jfdt;
+
+import org.jtrfp.jtrfp.raw.AbstractIRawDataTest;
+import org.jtrfp.jtrfp.raw.IRawData;
+
+public class RAWAdapterTest extends AbstractIRawDataTest {
+
+    @Override
+    protected IRawData getSubject() {
+	try {return new RAWAdapter(new RAWFile(getInputStream()));}
+	catch(Throwable t) {throw new RuntimeException(t);}
+    }
+
+}//end RawAdapterTest
