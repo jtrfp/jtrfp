1e744948af4fdb1afc2c7d619460ee3eb81c5715
Chuck Ritola <cjritola@gmail.com>
2024-05-08 13:45:27 -0400

    Migrate RaceType from trcl.

---
+++ src/main/java/org/jtrfp/jtrfp/sit/jfdt/RaceType.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * This file is part of jTRFP
+ * Copyright (c) 2012-2024. See commit history for copyright owners.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial implementation
+ ******************************************************************************/
+
+package org.jtrfp.jtrfp.sit.jfdt;
+
+/**
+ * Initial values adapted from forum post: http://forum.mtm2.com/viewtopic.php?t=1832&highlight=sit+file
+ * @author Chuck Ritola
+ *
+ */
+public enum RaceType {
+    HillClimb,
+    Drag,
+    Race,
+    Rally,
+    Rumble,
+    unknown5,
+    unknown6,
+    unknown7
+}
